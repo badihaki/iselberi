3a350d6eecdc03004dc4f708f4f0d091f981363d
B. Haki Martin <jojackblack@gmail.com>
2024-05-25 11:24:21 -0400

    server runs

--- server/app.js
+++ server/app.js
@@ -4,3 +4,7 @@ const port = 3000;
 app.get("/hello", (req, res)=>{
     res.send("helluuuurrr wrld");
 })
+
+app.listen(port, (connection)=>{
+    console.log(`started listening on port ${port}`);
+})
