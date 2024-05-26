882b3b9b406f844d7c2b35323499b0673fdd58f2
B. Haki Martin <jojackblack@gmail.com>
2024-05-26 00:43:28 -0400

    created the models directory, and a user model. Installed Argon2. Refactored code in app.js, more easily shows where middleware, routes, and routing middleware live

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-const app = require("express")();
+const express = require("express");
 const mongoose = require("mongoose");
 const port = 3000;
+const app = express();
 
 mongoose.connect("mongodb+srv://jojackblack:VaZ0t4CIOhjp1eBL@cluster0.yysbreu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
+// routes
 const auth = require("./controllers/auth");
 
-app.get("/hello", (req, res)=>{
-    res.send("helluuuurrr wrld");
-})
+// middleware
+app.use(express.json());
 
+// routing middleware
 app.use("/auth", auth);
 
 app.listen(port, (connection)=>{
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const argon = require("argon2");
 
 router.get("/", (req, res)=>{
     res.send("hey from auth controller");
+})
+
+router.post("/signup", (req, res)=>{
+    console.log(req.body);
+    res.send(`Good stuff`)
 })
 
 module.exports = router;
