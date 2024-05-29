45d1ebae622b9c2180996e847e55c1ed14f26453
B. Haki Martin <jojackblack@gmail.com>
2024-05-29 12:02:58 -0400

    users are saving to database with username, email and password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,32 @@
 const router = require("express").Router();
+const userModel = require("../models/User");
 const argon = require("argon2");
 
 router.get("/", (req, res)=>{
     res.send("hey from auth controller");
 })
 
-router.post("/signup", (req, res)=>{
-    console.log(req.body);
-    res.send(`Good stuff`)
+router.post("/signup", async (req, res)=>{
+    // console.log(req.body);
+    const { email, password, confirmPassword, username } = req.body;
+    
+    if(password === confirmPassword){
+        try{
+            const hash = await argon.hash(password);
+            const newUser = new userModel({
+                username, email, password:hash
+            })
+            // console.log(newUser);
+            await newUser.save();
+            res.status(200).send(newUser);
+        }
+        catch{
+            res.status(400).send({"err": "Cant make a hashed password"});
+        }
+    }
+    else{
+        res.status(400).send("bad stuff");
+    }
 })
 
 module.exports = router;
