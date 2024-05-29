96e82d0f3a9d0c97746eb65d3246c0f9f99c5b01
B. Haki Martin <jojackblack@gmail.com>
2024-05-29 14:46:43 -0400

    changed how user data is sent back to client. Client sends form data to server with validations

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from './services/user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -11,6 +13,10 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  api:string = "http://localhost:3000/auth/signup";
+  userService:UserService = inject(UserService);
+  http:HttpClient = inject(HttpClient);
+
   signUpForm:FormGroup = new FormGroup({
     username: new FormControl("", [
       Validators.required
@@ -28,14 +34,27 @@ export class SignupComponent {
   })
 
   submitForm():void {
-    console.log("chekc out this form");
-    console.log(this.signUpForm.value);
-    if(this.signUpForm.get("passwordConfirmation") != this.signUpForm.get("password")){
+    // console.log("chekc out this form");
+    // console.log(this.signUpForm.value);
+    if(this.signUpForm.value.passwordConfirmation != this.signUpForm.value.password){
       this.addError("Passwords do not match");
       this.startErrorTimer();
     }
     else{
-      // 
+      if(this.signUpForm.valid){
+        console.log("made it to signup")
+        this.http.post(this.api, this.signUpForm.value).subscribe({
+          next: (data)=>{
+             console.log(data);
+          },
+          error: (err)=>{
+            console.log(err);
+          },
+          complete: ()=>{
+            console.log("done with sign up flow");
+          }
+        });
+      }
     }
     this.signUpForm.reset();
   }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const { json } = require("express");
 const userModel = require("../models/User");
 const argon = require("argon2");
+const User = require("../models/User");
 
 router.get("/", (req, res)=>{
     res.send("hey from auth controller");
@@ -10,6 +12,12 @@ router.post("/signup", async (req, res)=>{
     // console.log(req.body);
     const { email, password, confirmPassword, username } = req.body;
     
+    const duplicateEmail = await User.find({email});
+
+    if(duplicateEmail){
+        res.status(400).send({error:"An account is already created using this email address"});
+        return;
+    }
     if(password === confirmPassword){
         try{
             const hash = await argon.hash(password);
@@ -18,14 +26,17 @@ router.post("/signup", async (req, res)=>{
             })
             // console.log(newUser);
             await newUser.save();
-            res.status(200).send(newUser);
+            res.status(200).send({username, email});
+            return;
         }
         catch{
-            res.status(400).send({"err": "Cant make a hashed password"});
+            res.status(400).send({error: "Cant make a hashed password"});
+            return;
         }
     }
     else{
-        res.status(400).send("bad stuff");
+        res.status(400).send({error:"password is incorrect"});
+        return;
     }
 })
 
