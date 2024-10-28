61553ae1b2685257dc26c44fdc2f1b965b5fa9c4
B. Haki Martin <jojackblack@gmail.com>
2024-10-28 16:08:54 -0400

    updated signup.tsx ui

--- src/lib/components/Auth/SignUp.tsx
+++ src/lib/components/Auth/SignUp.tsx
@@ -3,6 +3,7 @@
 import React, { ChangeEvent, useState } from 'react'
 import Form from "next/form"
 import { Button } from '@/components/ui/button';
+import { Stack } from '@chakra-ui/react';
 
 function SignUp() {
     interface ISignUpForm{
@@ -32,11 +33,42 @@ function SignUp() {
 
     return (
         <div>
-            Sign Up
-            <Form action={onSubmit}>
-                <input name='username' onChange={onChange} value={form.username} />
-                <Button type='submit'>Sign Up</Button>
-            </Form>
+            <Stack align={'center'}
+            className='bg-stone-400 bg-opacity-20 mx-auto'
+            >
+                <div className='font-bold text-lg'>
+                    Sign Up
+                </div>
+                <Form action={onSubmit}>
+                    <Stack spaceY={4} align={'center'}>
+                        <div id='sign-up-username'>
+                            <span className='mr-4'>
+                                Username:
+                            </span>
+                            <input name='username' onChange={onChange} value={form.username} />
+                        </div>
+                        <div id='sign-up-email'>
+                            <span className='mr-4'>
+                                Email:
+                            </span>
+                            <input name='email' onChange={onChange} value={form.email} />
+                        </div>
+                        <div id='sign-up-password'>
+                            <span className='mr-4'>
+                                Password:
+                            </span>
+                            <input name='username' onChange={onChange} value={form.password} />
+                        </div>
+                        <div id='sign-up-confirm'>
+                            <span className='mr-4'>
+                                Retype to Confirm Your Password
+                            </span>
+                            <input name='username' onChange={onChange} value={form.confirmPass} />
+                        </div>
+                        <Button type='submit'>Sign Up</Button>
+                    </Stack>
+                </Form>
+            </Stack>
         </div>
     )
 }
