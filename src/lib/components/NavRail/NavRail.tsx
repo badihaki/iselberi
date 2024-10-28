e951c19c907df73f5c577d08623e95eb2a75fa31
B. Haki Martin <jojackblack@gmail.com>
2024-10-28 13:31:18 -0400

    added a drawer

--- src/lib/components/NavRail/NavRail.tsx
+++ src/lib/components/NavRail/NavRail.tsx
@@ -1,10 +1,25 @@
+import { Button } from '@/components/ui/button';
+import { DrawerBackdrop, DrawerBody, DrawerCloseTrigger, DrawerContent, DrawerFooter, DrawerHeader, DrawerRoot, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
 import React from 'react'
 
 function NavRail() {
   return (
-    <div>
-      Nav Rail
-    </div>
+    <DrawerRoot size={'xs'}>
+      <DrawerBackdrop />
+      <DrawerTrigger asChild>
+        <Button variant={'outline'} size={"md"} backgroundColor={'slateblue'} margin={4} padding={2} fontSize={22}>
+          Navigation
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent offset={4} rounded="md">
+        <DrawerCloseTrigger />
+        <DrawerHeader>
+          <DrawerTitle />
+        </DrawerHeader>
+        <DrawerBody />
+        <DrawerFooter />
+      </DrawerContent>
+    </DrawerRoot>
   )
 }
 
