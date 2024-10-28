8c9bba311eb3ff12dd740e0ac57c6c8b165500b8
B. Haki Martin <jojackblack@gmail.com>
2024-10-28 14:01:09 -0400

    user section of navigation is being constructed

--- src/lib/components/NavRail/NavRail.tsx
+++ src/lib/components/NavRail/NavRail.tsx
@@ -1,13 +1,32 @@
-import { Button } from '@/components/ui/button';
-import { DrawerBackdrop, DrawerBody, DrawerCloseTrigger, DrawerContent, DrawerFooter, DrawerHeader, DrawerRoot, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import Image from "next/image";
+import React from "react";
+import userIcon from "@/lib/graphics/icons/user-round.svg";
 
 function NavRail() {
   return (
-    <DrawerRoot size={'xs'}>
+    <DrawerRoot size={"xs"} placement={"start"}>
       <DrawerBackdrop />
       <DrawerTrigger asChild>
-        <Button variant={'outline'} size={"md"} backgroundColor={'slateblue'} margin={4} padding={2} fontSize={22}>
+        <Button
+          variant={"outline"}
+          size={"md"}
+          backgroundColor={"slateblue"}
+          margin={4}
+          padding={2}
+          fontSize={22}
+        >
           Navigation
         </Button>
       </DrawerTrigger>
@@ -16,11 +35,27 @@ function NavRail() {
         <DrawerHeader>
           <DrawerTitle />
         </DrawerHeader>
-        <DrawerBody />
+        <DrawerBody>
+          <div
+            id="area-user"
+            className="m-4 bg-stone-500 bg-opacity-10 text-center my-auto py-4 px-2"
+          >
+            <Image
+              src={userIcon}
+              alt="User Icon"
+              width={50}
+              height={50}
+              className="bg-slate-100 bg-opacity-40 rounded-full pb-2"
+            />
+            <div id="user-text" className="font-extrabold">
+              User
+            </div>
+          </div>
+        </DrawerBody>
         <DrawerFooter />
       </DrawerContent>
     </DrawerRoot>
-  )
+  );
 }
 
 export default NavRail;
