db8d929b65b7603ae16c3cefdad12a1d8b11cda1
B. Haki Martin <jojackblack@gmail.com>
2024-10-28 15:55:38 -0400

    signup form works with usestate hook

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,13 @@
+import LogIn from '@/lib/components/Auth/LogIn'
+import SignUp from '@/lib/components/Auth/SignUp'
 import React from 'react'
 
 function Auth() {
   return (
-    <div>
+    <div className='ml-24 mr-6 p-2 bg-stone-300 bg-opacity-10'>
       Auth
+      <SignUp />
+      <LogIn />
     </div>
   )
 }
--- src/lib/components/Auth/SignUp.tsx
+++ src/lib/components/Auth/SignUp.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+"use client"
+
+import React, { ChangeEvent, useState } from 'react'
+import Form from "next/form"
+import { Button } from '@/components/ui/button';
 
 function SignUp() {
-  return (
-    <div>
-      Sign Up
-    </div>
-  )
+    interface ISignUpForm{
+        username:string,
+        email:string,
+        password:string,
+        confirmPass:string,
+    }
+    const [form, setForm] = useState<ISignUpForm>({
+        username:"",
+        email:"",
+        confirmPass:"",
+        password:""
+    })
+    
+    function onChange(ev:ChangeEvent<HTMLInputElement>){
+        console.log(ev.target.value);
+        const update = {...form};
+        update[ev.target.name as keyof typeof update] = ev.target.value;
+        console.log(update);
+        setForm(update);
+    }
+
+    function onSubmit(formData:FormData){
+        console.log(formData);
+    }
+
+    return (
+        <div>
+            Sign Up
+            <Form action={onSubmit}>
+                <input name='username' onChange={onChange} value={form.username} />
+                <Button type='submit'>Sign Up</Button>
+            </Form>
+        </div>
+    )
 }
 
 export default SignUp
